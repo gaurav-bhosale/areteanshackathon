b7056d384e6ca3dfdd7e102fd1f88766311e8a84
gaurav-bhosale <58224133+gaurav-bhosale@users.noreply.github.com>
2020-12-16 16:34:47 +0530

    third commit

--- Bank/src/com/areteans/servlet/ListAllCustomers.java
+++ Bank/src/com/areteans/servlet/ListAllCustomers.java
@@ -38,17 +38,39 @@ public class ListAllCustomers extends HttpServlet{
 	}
 	
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
 			throws ServletException, IOException {
 		
 		PrintWriter out = resp.getWriter();
 		
 		try {
 			ResultSet rs = pStmt.executeQuery();
-			
+			out.println("<!DOCTYPE html>\r\n" + 
+					"<html>\r\n" + 
+					"<head>\r\n" + 
+					"<meta charset=\"ISO-8859-1\">\r\n" + 
+					"<title>Insert title here</title>\r\n" + 
+					"</head>\r\n" + 
+					"	<body>\r\n" + 
+					"		<table>\r\n" + 
+					"			<tr>\r\n" + 
+					"				<th> Account Number </th>\r\n" + 
+					"				<th> Account Type </th>\r\n" + 
+					"				<th> Balance </th>\r\n" + 
+					"			</tr>\r\n" + 
+					"			<tr>");
 			while(rs.next()) {
-				
+				out.println("<td>"+rs.getInt(1)+"</td>"+
+						"<td>"+rs.getInt(4)+"</td>"+
+						"<td>"+rs.getInt(9)+"</td>"+
+						"<td><a href=\"Credit?id=1\">Credit</a></td>\r\n" + 
+						"<td><a href=\"Debit?id=1\">Debit</a></td>\r\n" + 
+						"<td><a href=\"GetDetails?id=1\">Details</a></td>");
 			}
+			out.println("</tr>\r\n" + 
+					"		</table>\r\n" + 
+					"	</body>\r\n" + 
+					"</html>");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
